82bae0c42d9f4c7b9f040c9859e752ea6c33f58f
cvedovini <cvedovini@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-05 07:49:26 +0000

    releasing 1.3.3, correcting the css version
    git-svn-id: https://plugins.svn.wordpress.org/wp-linkedin/tags/1.3.3@708140 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-linkedin.php
+++ wp-linkedin.php
@@ -5,7 +5,7 @@ Plugin URI: http://vedovini.net/plugins/?utm_source=wordpress&utm_medium=plugin&
 Description: This plugin enables you to add various part of your LinkedIn profile to your Wordpress blog.
 Author: Claude Vedovini
 Author URI: http://vedovini.net/?utm_source=wordpress&utm_medium=plugin&utm_campaign=wp-linkedin
-Version: 1.2.0
+Version: 1.3.3
 
 # The code in this plugin is free software; you can redistribute the code aspects of
 # the plugin and/or modify the code under the terms of the GNU Lesser General
@@ -30,14 +30,19 @@ define('LINKEDIN_USERTOKEN', get_option('wp-linkedin_usertoken'));
 define('LINKEDIN_USERSECRET', get_option('wp-linkedin_usersecret'));
 
 define('LINKEDIN_FIELDS_BASIC', 'id, first-name, last-name, picture-url, headline, location, industry, public-profile-url');
-define('LINKEDIN_FIELDS_DEFAULT', 'summary, specialties, languages, skills, educations, positions, recommendations-received');
+define('LINKEDIN_FIELDS_RECOMMENDATIONS', 'recommendations-received:(recommendation-text,recommender:(first-name,last-name,public-profile-url))');
+define('LINKEDIN_FIELDS_DEFAULT', 'summary, specialties, languages, skills, educations, positions, ' . LINKEDIN_FIELDS_RECOMMENDATIONS);
 define('LINKEDIN_FIELDS', get_option('wp-linkedin_fields', LINKEDIN_FIELDS_DEFAULT));
+define('LINKEDIN_PROFILELANGUAGE', get_option('wp-linkedin_profilelanguage'));
+
+include 'class-recommendations-widget.php';
 
 
 class WPLinkedInPlugin {
 
 	function WPLinkedInPlugin() {
 		add_action('init', array(&$this, 'init'));
+		add_action('widgets_init', array(&$this, 'widgets_init'));
 	}
 
 	function init() {
@@ -45,19 +50,20 @@ class WPLinkedInPlugin {
 		// Translations can be filed in the /languages/ directory
 		load_plugin_textdomain('wp-linkedin', false, dirname(plugin_basename(__FILE__)) . '/languages' );
 
-		wp_register_sidebar_widget('wp-linkedin-recommendation-widget', 'LinkedIn Recommendations',
-				array(&$this, 'recommendations_widget'), array('description' => __('A slider with your LinkedIn recommendations', 'wp-linkedin')));
-		wp_register_widget_control('wp-linkedin-recommendation-widget', 'LinkedIn Recommendations',
-				array(&$this, 'recommendations_widget_control'), array('id_base' => 'wp-linkedin-recommendation-widget'));
-
 		if (is_admin()) {
 			add_action('admin_menu', array(&$this, 'admin_menu'));
 		} else {
 			wp_register_script('jquery.tools', 'http://cdn.jquerytools.org/1.2.7/all/jquery.tools.min.js', array('jquery'), '1.2.7');
+			wp_register_script('jquery-dimension-etc', plugins_url('wp-linkedin/jquery.dimensions.etc.min.js'), array('jquery'), '1.0.0');
+			wp_register_style('wp-linkedin', plugins_url('wp-linkedin/style.css'), false, '1.0.2');
 			add_action('wp_enqueue_scripts', array(&$this, 'enqueue_scripts'));
-			add_shortcode('li_recommendations', array(&$this, 'recommendations_sc'));
-			add_shortcode('li_profile', array(&$this, 'profile_sc'));
+			add_shortcode('li_recommendations', 'wp_linkedin_recommendations');
+			add_shortcode('li_profile', 'wp_linkedin_profile');
 		}
+	}
+
+	function widgets_init() {
+		register_widget('WPL_Recommendations_Widget');
 	}
 
 	function admin_menu() {
@@ -65,83 +71,74 @@ class WPLinkedInPlugin {
 		$this->admin = new WPLinkedInAdmin($this);
 	}
 
-	function recommendations_widget() {
-		$options = get_option('recommendations_widget_options');
-		echo $this->recommendations_sc($options);
-	}
-
-	function recommendations_widget_control() {
-		$options = get_option('recommendations_widget_options');
-
-		if (!is_array( $options )) {
-			$options = array(
-				'width' => '480',
-				'length' => '200',
-				'interval' => '1000'
-			);
-		}
-
-		if (isset($_POST['width'])) {
-			$options['width'] = $_POST['width'];
-		}
-
-		if (isset($_POST['length'])) {
-			$options['length'] = $_POST['length'];
-		}
-
-		if (isset($_POST['interval'])) {
-			$options['interval'] = $_POST['interval'];
-		}
-
-		if (!isset($options['interval'])) $options['interval'] = '1000';
-
-		update_option('recommendations_widget_options', $options);
-
-		echo '<p><label>Width of widget (in px): </label><br/>';
-		echo '<input type="text" name="width" value="'.$options['width'].'" size="4" /></p>';
-		echo '<p><label>Length of recommendations (in char): </label><br/>';
-		echo '<input type="text" name="length" value="'.$options['length'].'" size="4" /></p>';
- 		echo '<p><label>Scroller\'s speed: </label><br/>';
-		echo '<input type="text" name="interval" value="'.$options['interval'].'" size="5" /></p>';
-	}
-
 	function enqueue_scripts() {
 		wp_enqueue_script('jquery.tools');
+		wp_enqueue_script('jquery-dimension-etc');
+		wp_enqueue_style('wp-linkedin', plugins_url('wp-linkedin/style.css'), false, '1.0.0');
 	}
+}
 
-	function profile_sc($atts) {
-		// In case they want to pass customized attribute to their custom template
-		extract(shortcode_atts(array(
-				'fields' => LINKEDIN_FIELDS
-				), $atts));
 
-		$fields = preg_replace('/\s+/', '', LINKEDIN_FIELDS_BASIC . ', ' . $fields);
+function wp_linked_get_profile($options='id', $lang=LINKEDIN_PROFILELANGUAGE) {
+	require_once 'linkedin_3.2.0.class.php';
 
-		$profile = $this->get_profile($fields);
-		$template = locate_template('linkedin/profile.php');
+	$API_CONFIG = array(
+			'appKey'       => LINKEDIN_APPKEY,
+			'appSecret'    => LINKEDIN_APPSECRET,
+			'callbackUrl'  => NULL
+	);
 
-		ob_flush();
-		ob_start();
-		if (!empty($template)) {
-			require $template;
-		} else {
-			require 'templates/profile.php';
-		}
-		$results = ob_get_contents();
-		ob_end_clean();
-		return $results;
+	$linkedin = new LinkedIn($API_CONFIG);
+	$linkedin->setTokenAccess(array('oauth_token' => LINKEDIN_USERTOKEN, 'oauth_token_secret' => LINKEDIN_USERSECRET));
+	$linkedin->setResponseFormat(LINKEDIN::_RESPONSE_JSON);
+	$linkedin->setProfileLanguage($lang);
+
+	$response = $linkedin->profile("~:($options)");
+
+	if($response['success'] === TRUE) {
+		return json_decode($response['linkedin']);
+	} else {
+		return false;
 	}
+}
 
-	function recommendations_sc($atts) {
-		extract(shortcode_atts(array(
-				'width' => '480',
-				'length' => '200',
-				'interval' => '1000'
-		), $atts));
 
-		$profile = $this->get_profile('recommendations-received');
+function wp_linkedin_profile($atts) {
+	// In case they want to pass customized attribute to their custom template
+	extract(shortcode_atts(array(
+			'fields' => LINKEDIN_FIELDS,
+			'lang' => LINKEDIN_PROFILELANGUAGE
+	), $atts));
+
+	$fields = preg_replace('/\s+/', '', LINKEDIN_FIELDS_BASIC . ', ' . $fields);
+
+	$profile = wp_linked_get_profile($fields, $lang);
+	$template = locate_template('linkedin/profile.php');
+
+	ob_flush();
+	ob_start();
+	if (!empty($template)) {
+		require $template;
+	} else {
+		require 'templates/profile.php';
+	}
+	$results = ob_get_contents();
+	ob_end_clean();
+	return $results;
+}
+
+
+function wp_linkedin_recommendations($atts) {
+	extract(shortcode_atts(array(
+			'width' => '480',
+			'length' => '200',
+			'interval' => '1000'
+	), $atts));
+
+	$profile = wp_linked_get_profile(LINKEDIN_FIELDS_RECOMMENDATIONS);
+
+	if (isset($profile->recommendationsReceived) && is_array($profile->recommendationsReceived->values)) {
 		$recommendations = $profile->recommendationsReceived->values;
-
 		$template = locate_template('linkedin/recommendations.php');
 
 		ob_flush();
@@ -155,30 +152,8 @@ class WPLinkedInPlugin {
 		ob_end_clean();
 		return $results;
 	}
-
-	function get_profile($options='id') {
-		require_once 'linkedin_3.2.0.class.php';
-
-		$API_CONFIG = array(
-				'appKey'       => LINKEDIN_APPKEY,
-				'appSecret'    => LINKEDIN_APPSECRET,
-				'callbackUrl'  => NULL
-		);
-
-		$linkedin = new LinkedIn($API_CONFIG);
-        $linkedin->setTokenAccess(array('oauth_token' => LINKEDIN_USERTOKEN, 'oauth_token_secret' => LINKEDIN_USERSECRET));
-        $linkedin->setResponseFormat(LINKEDIN::_RESPONSE_JSON);
-
-		$response = $linkedin->profile("~:($options)");
-
-        if($response['success'] === TRUE) {
-        	return json_decode($response['linkedin']);
-        } else {
-        	return false;
-        }
-	}
-
 }
+
 
 function wp_linkedin_excerpt($str, $length) {
 	$length++;
@@ -197,6 +172,7 @@ function wp_linkedin_excerpt($str, $length) {
 		echo $str;
 	}
 }
+
 
 global $the_wp_linked_plugin;
 $the_wp_linked_plugin = new WPLinkedInPlugin();
