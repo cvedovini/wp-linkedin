469a2544b7ef3d95d1cbe265423beff8c61bb44d
cvedovini <cvedovini@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-24 19:36:29 +0000

    added Catalan transaltions
    git-svn-id: https://plugins.svn.wordpress.org/wp-linkedin/tags/1.12@864253 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/network-updates.php
+++ templates/network-updates.php
@@ -24,49 +24,58 @@ if (!function_exists('li_find_links')) {
 ?>
 <div class="linkedin"><ul class="updates">
 <?php foreach ($updates->values as $update):
-$p = isset($update->updateContent->person) ? $update->updateContent->person : '';
+if (!isset($update->updateContent->person)) continue;
+$p = $update->updateContent->person;
 if ($p->firstName == 'private') continue;
+$profile_name = li_profile_name($p);
 
 switch ($update->updateType) {
 	case 'CONN':
+		$conn = array();
 		foreach ($p->connections->values as $c) {
-			echo '<li class="type-'.strtolower($update->updateType) . '">';
-			printf(__('%1$s is now connected to %2$s.', 'wp-linkedin'), li_profile_name($p), li_profile_name($c));
-			echo '</li>';
+			$conn[] = li_profile_name($c);
 		}
+		echo '<li class="type-'.strtolower($update->updateType) . '">';
+		printf(__('%1$s is now connected to %2$s.', 'wp-linkedin'), $profile_name, implode(', ', $conn));
+		echo '</li>';
+
 		break;
 	case 'NCON':
 		echo '<li class="type-'.strtolower($update->updateType) . '">';
-		printf(__('%s is now a connection.', 'wp-linkedin'), li_profile_name($p));
+		printf(__('%s is now a connection.', 'wp-linkedin'), $profile_name);
 		echo '</li>';
 		break;
 	case 'CCEM':
 		echo '<li class="type-'.strtolower($update->updateType) . '">';
-		printf(__('%s has joined LinkedIn.', 'wp-linkedin'), li_profile_name($p));
+		printf(__('%s has joined LinkedIn.', 'wp-linkedin'), $profile_name);
 		echo '</li>';
 		break;
 	case 'SHAR':
 		echo '<li class="type-'.strtolower($update->updateType) . '">';
-		printf(__('%1$s says: %2$s', 'wp-linkedin'), li_profile_name($p), li_find_links($p->currentShare->comment));
+		printf(__('%1$s says: %2$s', 'wp-linkedin'), $profile_name, li_find_links($p->currentShare->comment));
 		echo '</li>';
 		break;
 	case 'STAT':
 		echo '<li class="type-'.strtolower($update->updateType) . '">';
-		printf(__('%1$s says: %2$s', 'wp-linkedin'), li_profile_name($p), li_find_links($p->currentStatus));
+		printf(__('%1$s says: %2$s', 'wp-linkedin'), $profile_name, li_find_links($p->currentStatus));
 		echo '</li>';
 		break;
 	case 'VIRL':
 		echo '<li class="type-'.strtolower($update->updateType) . '">';
-		printf(__('%1$s likes: %2$s', 'wp-linkedin'), li_profile_name($p),
+		printf(__('%1$s likes: %2$s', 'wp-linkedin'), $profile_name,
 				li_find_links($p->updateAction->originalUpdate->updateContent->currentShare->comment));
 		echo '</li>';
 		break;
 	case 'JGRP':
+		$groups = array();
 		foreach ($p->memberGroups->values as $g) {
-			echo '<li class="type-'.strtolower($update->updateType) . '">';
-			printf(__('%1$s joined the group %2$s.', 'wp-linkedin'), li_profile_name($p), $g->name);
-			echo '</li>';
+			$groups[] = $g->name;
 		}
+		echo '<li class="type-'.strtolower($update->updateType) . '">';
+		printf(_n('%1$s joined the group %2$s.', '%1$s joined the groups %2$s.', count($groups), 'wp-linkedin'),
+				$profile_name, implode(', ', $groups));
+		echo '</li>';
+
 		break;
 	case 'APPS':
 	case 'APPM':
@@ -78,36 +87,38 @@ switch ($update->updateType) {
 		break;
 	case 'PICU':
 		echo '<li class="type-'.strtolower($update->updateType) . '">';
-		printf(__('%s has a new profile picture.', 'wp-linkedin'), li_profile_name($p));
+		printf(__('%s has a new profile picture.', 'wp-linkedin'), $profile_name);
 		echo '</li>';
 		break;
 	case 'PROF':
 	case 'PRFU':
 	case 'PRFX':
 		echo '<li class="type-'.strtolower($update->updateType) . '">';
-		printf(__('%s has an updated profile.', 'wp-linkedin'), li_profile_name($p));
+		printf(__('%s has an updated profile.', 'wp-linkedin'), $profile_name);
 		echo '</li>';
 		break;
 	case 'PREC':
 	case 'SVPR':
+		$reco = array();
 		foreach ($p->recommendationsGiven->values as $r) {
-			echo '<li class="type-'.strtolower($update->updateType) . '">';
-			printf(__('%1$s recommends %2$s.', 'wp-linkedin'), li_profile_name($p), li_profile_name($r->recommendee));
-			echo '</li>';
+			$reco[] = li_profile_name($r->recommendee);
 		}
+		echo '<li class="type-'.strtolower($update->updateType) . '">';
+		printf(__('%1$s recommends %2$s.', 'wp-linkedin'), $profile_name, implode(', ', $reco));
+		echo '</li>';
 		break;
 	case 'JOBP':
 		$j = $update->updateContent->job;
 		$p = $j->jobPoster;
 		echo '<li class="type-'.strtolower($update->updateType) . '">';
-		printf(__('%1$s posted a job: %2$s at %3$s.', 'wp-linkedin'), li_profile_name($p),
+		printf(__('%1$s posted a job: %2$s at %3$s.', 'wp-linkedin'), $profile_name,
 				$j->position->title, $j->company->name);
 		echo '</li>';
 		break;
 	case 'MSFC':
 		$p = $update->updateContent->companyPersonUpdate->person;
 		echo '<li class="type-'.strtolower($update->updateType) . '">';
-		printf(__('%1$s is now following %2$s.', 'wp-linkedin'), li_profile_name($p),
+		printf(__('%1$s is now following %2$s.', 'wp-linkedin'), $profile_name,
 				$update->updateContent->company->name);
 		echo '</li>';
 		break;
@@ -118,9 +129,9 @@ switch ($update->updateType) {
 		echo '</li>';
 		break;
 	default:
-		echo '<li class="type-'.strtolower($update->updateType) . '">';
-		echo $update->updateType;
-		echo '</li>';
+		echo "<!--\n";
+		print_r($update);
+		echo "\n-->";
 }
 ?>
 <?php endforeach; ?>
